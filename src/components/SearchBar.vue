87a7abd82f2ee2f3d66f64888121ba19d0f47038
SparksFullStack <csparks@antioch.edu>
2018-10-24 11:49:00 -0700

    search bar fully functional--working on integrating the Youtube API next

--- src/components/SearchBar.vue
+++ src/components/SearchBar.vue
@@ -1,12 +1,17 @@
 <template>
     <div>
-        <input />
+        <input @input="onInput" />
     </div>
 </template>
 
 <script>
 export default {
     name: 'SearchBar',
+    methods: {
+        onInput(e) { 
+            this.$emit('termChange', e.target.value);
+        },
+    }
 }
 </script>
 
