74cbacd31534958c06d12c5d1ef4e4f643a33569
SparksFullStack <csparks@antioch.edu>
2018-10-24 13:22:32 -0700

    looping over data returned by the Youtube API; created a component that renders for each video stored in data

--- src/components/VideoList.vue
+++ src/components/VideoList.vue
@@ -1,12 +1,27 @@
+/* eslint-disable */
+
 <template>
     <ul>
-        Video List
+        <VideoListItem 
+            v-for="video in videos"
+            :video="video"
+            :key="video.etag"    
+        >
+        </VideoListItem>
     </ul>
 </template>
 
 <script>
+import VideoListItem from './VideoListItem'
+
 export default{
     name: 'VideoList',
+    props: {
+        videos: Array,
+    },
+    components: {
+        VideoListItem,
+    }
 }
 </script>
 
