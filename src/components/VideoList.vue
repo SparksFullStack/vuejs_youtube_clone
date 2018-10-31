e8a140ae47b2de253e038454c3cfe43cd687cdf9
SparksFullStack <csparks@antioch.edu>
2018-10-31 13:00:45 -0700

    video embed working and styled

--- src/components/VideoList.vue
+++ src/components/VideoList.vue
@@ -1,7 +1,7 @@
 /* eslint-disable */
 
 <template>
-    <ul class='list-group'>
+    <ul class='list-group col-md-4'>
         <VideoListItem 
             v-for="video in videos"
             :video="video"
